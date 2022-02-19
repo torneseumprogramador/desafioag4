94356e0e80c55a67703996f4c3d38c8331535554
Maria <maria.a.b.lima@gmail.com>
2022-02-19 19:00:37 -0300

    Criação da tabela com arquivos dos clientes

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
-
+import pyodbc 
 import csv
 import glob
+import datetime
 
 soIn = glob.glob("arquivos_carga_csv/transaction-in-*.csv")
 soOut = glob.glob("arquivos_carga_csv/transaction-out-*.csv")
 soCliente = glob.glob("arquivos_carga_csv/clients-*.csv")
+
 count =0
-for arq in soIn:    
-    # print(arq)
+for arq in soIn: 
+     # print(arq)
     ficheiro = open(arq, 'r', encoding="utf8")  
     reader = csv.reader(ficheiro)
     for linha in reader:
@@ -23,16 +25,40 @@ for arq in soOut:
         count2 += 1
         print ( count2)
 
-count3 = 0        
-for arq in soCliente:
+connection = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=DESKTOP-PO3SOQV;'
+                      'Database=Desafio;'
+                      'Trusted_Connection=yes;')
+cursor = connection.cursor()
+
+cursor.execute('''
+		CREATE TABLE clientes (
+			id int primary key,
+			nome nvarchar(50),
+			email nvarchar(100),
+            data_cadastro datetime,
+            telefone nvarchar(50),
+			)
+               ''')
+
+# Inserindo as linhas dos arquivos do cliente na Tabela
+for arq in soCliente: 
     # print(arq)
     ficheiro = open(arq, 'r', encoding="utf8")
     reader = csv.reader(ficheiro)
     for linha in reader:
-        count3 += 1
-        print ( count3)     
-           
-total=count+count2+count3 
-
-print(total)
+        if 'id;nome' in linha[0]: continue
+        quebralinha=linha[0].split(';')
+        date_time_obj = datetime.datetime.strptime( quebralinha[3], '%Y-%m-%d %H:%M:%S %z')
+        cursor.execute('''
+                    INSERT INTO clientes (id, nome, email, data_cadastro, telefone)
+                    VALUES (?,?,?,?,?)
+                    ''',
+                   quebralinha[0], 
+                   quebralinha[1], 
+                   quebralinha[2], 
+                   date_time_obj, 
+                   quebralinha[4],
+                    )
+connection.commit()
 
