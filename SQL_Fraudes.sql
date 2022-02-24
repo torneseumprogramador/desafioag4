42f5b1971cc48facea03e6c53e3ef07df792f032
Ellen Kindermann <ellen-kin@hotmail.com>
2022-02-24 13:22:08 -0300

    detectando fraudes SQL

--- SQL_Fraudes.sql
+++ SQL_Fraudes.sql
@@ -2,12 +2,12 @@ SELECT sdiff.cliente_id
   , sdiff.valor
   , sdiff.data
   , sdiff.transOut_id
-  , sdiff.minute_diff
+  , sdiff.second_diff
 FROM (
 	  select cliente_id, valor, data, id as transOut_id,
-	datediff(MINUTE, lag(data,1) over (partition by cliente_id order by data asc), data) as 'minute_diff'
+	datediff(SECOND, lag(data,1) over (partition by cliente_id order by data asc), data) as 'second_diff'
 	from transactionout
 ) sdiff
-WHERE sdiff.minute_diff<2
+WHERE sdiff.second_diff<120
 order by cliente_id
 
